1495dd18b6de14e983fc8be7fec787270aa35c9c
Your GitHub Username <sachinkumar843sk@gmail.com>
2024-04-27 00:37:47 +0530

    Initial commit

---
+++ lib/Views/splashScreen.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+import 'loginScreen.dart';
+
+class SplashScreen extends StatefulWidget {
+
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+
+  void initState() {
+    Timer(const Duration(seconds: 2), () async{
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => LogInScreen()));
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor:Colors.white,
+      body: Center(
+        child: Image.asset("assets/logos/logo.png"),
+      ),
+    );
+  }
+}
